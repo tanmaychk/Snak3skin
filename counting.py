06257a92a634b8db3f6dcc9754b5d7e996476b9e
Tanmay Chakrabarty <tanmay23c@gmail.com>
2020-10-16 13:16:52 +0530

    enter

--- counting.py
+++ counting.py
@@ -1,6 +1,6 @@
-wor = 0
-print(" before" , wor)
+cnt = 0
+print(" before" , cnt)
 for num in [9 , 42, 35, 67 ,89, 76]:
-    wor =wor + 1
+    cnt =cnt + 1
    
-print ("after" ,wor)
+print ("after" ,cnt)
