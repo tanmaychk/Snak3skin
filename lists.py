c5fc5cff50a095a6fda98fb366260747a168296c
Tanmay Chakrabarty <70465237+tanmayc0des@users.noreply.github.com>
2020-11-13 12:10:22 +0530

    Added Lists and range (DS)

--- lists.py
+++ lists.py
@@ -1,9 +1,43 @@
+# Changing items in a list
 items = [23, 24, 56, 79]
 print(items)
 items[1] = 45
 print(items)
-
+######################
 my_list = [1, 2, 3]
 print(my_list)
 my_list[2] = 4
 print(my_list)
+
+# adding items to a list (using while)
+emp = list()
+while True:
+    name = input("whats your name? : ")
+    emp.append(name)
+    if name == "done":
+        break
+print(emp)
+
+# counting stuff in a list
+length = len(emp)  # counts number of items
+counting = emp.count("lori")  # counts specific items
+print(length, counting)
+
+# sorting a list
+emp.sort()
+print(emp)
+
+# splitting to form a list
+st = " im ready to be splitted out "
+pieces = st.split()
+print(pieces)
+
+# math ops in lists
+# max .  min . sum . avg = sum/len
+number = [1, 2, 3, 5, 6, 93, 57]
+ma = max(number)
+mi = min(number)
+le = len(number)
+su = sum(number)
+avg = su/le
+print(ma, mi, le, su, avg)
---
+++ range.py
@@ -0,0 +1,3 @@
+x = range(6)
+for n in x:
+    print(n)
