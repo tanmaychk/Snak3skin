3ab7aa42709fe808c3afe381a3aede74b0311ae4
Tanmay Chakrabarty <70465237+tanmayc0des@users.noreply.github.com>
2020-11-14 13:02:41 +0530

    commit

---
+++ all-inclusive-usage.py
@@ -0,0 +1,27 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+lst = list()
+
+for line in handle:
+    if not line.startswith("From:"):
+        continue
+    line = line.split()
+    lst.append(line[1])
+
+counts = dict()
+
+for word in lst:
+    counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+
+
+print(bigword, bigcount)
