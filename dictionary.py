e35431f66f9b66ba1bb368e8269d69d1839ea853
Tanmay Chakrabarty <70465237+tanmayc0des@users.noreply.github.com>
2020-11-14 10:50:09 +0530

    added get

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,11 @@ bag["bottle "] = 4
 bag["card"] = 67
 bag["tiffin"] = 53
 print(bag)
+
+
+# simplified counting with "get"
+count = dict()
+names = ["csev", "name", "csev", "cane", "cane", "cane", "name"]
+for name in names:
+    count[name] = count.get(name, 0) + 1
+print(count)
